7ae9b6253b078bb2c0d1b22ae43b1ff7fb38459a
sunisonfire <ykuremysun@gmail.com>
2026-05-24 00:38:01 -0500

    create: about us is ready, with responsive

---
+++ aboutus.js
@@ -0,0 +1,12 @@
+const bus = document.getElementById("bus");
+const bocina = document.getElementById("bocina");
+
+bus.addEventListener("mouseenter", () => { 
+  bocina.currentTime = 0;
+  bocina.play();
+});
+
+bus.addEventListener("mouseleave", () => {  
+  bocina.pause();
+  bocina.currentTime = 0;
+});
